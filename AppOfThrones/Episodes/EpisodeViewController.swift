3455312911a31233d17c2d09383ae3abfc9078e2
Fran González <frangonzalezr@me.com>
2020-02-14 00:49:22 +0100

    Terminamos la primera semana

--- AppOfThrones/Episodes/EpisodeViewController.swift
+++ AppOfThrones/Episodes/EpisodeViewController.swift
@@ -12,8 +12,28 @@ class EpisodeViewController: UIViewController {
     
     var episodes: [Episode] = [Episode.init(id: 1, name: "Winter is coming", date: "13/2/2020", image: "", episode: 1, season: 1, overview: "Winter is coming")]
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupUI()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    func setupUI() {
         self.title = "Seasons"
     }
     
