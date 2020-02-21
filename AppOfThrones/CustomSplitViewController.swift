8f5dbaf298309c56ca0996fdc27a4abb893ef58b
frangonzalezr <frangonzalezr@gmail.com>
2020-02-21 20:53:01 +0100

    Añado animación Twitter

--- AppOfThrones/CustomSplitViewController.swift
+++ AppOfThrones/CustomSplitViewController.swift
@@ -2,8 +2,8 @@
 //  CustomSplitViewController.swift
 //  AppOfThrones
 //
-//  Created by Fernando Torcelly Garcia on 10/02/2020.
-//  Copyright © 2020 Fernando Torcelly Garcia. All rights reserved.
+//  Created by Fran González on 21/02/2020.
+//  Copyright © 2020 Fran González. All rights reserved.
 //
 
 import UIKit
@@ -13,10 +13,34 @@ class CustomSplitViewController: UISplitViewController,
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadTwitterSplashAnimation()
         self.delegate = self
         self.preferredDisplayMode = .allVisible
     }
-
+    
+    func loadTwitterSplashAnimation() -> Void {
+        let splashView = SplashView(iconImage: UIImage(named: "icon_1024")!,iconInitialSize: CGSize(width: 70, height: 70), backgroundColor: UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1))
+        
+        self.view.addSubview(splashView)
+        
+        splashView.duration = 2.0
+        splashView.animationType = AnimationType.twitter
+        splashView.iconColor = UIColor.black
+        splashView.useCustomIconColor = false
+        
+        splashView.startAnimation(){
+            print("Completed")
+        }
+        
+        // This will deliver the message to stop the heartbeat
+        // Need to call like same after the completion of API, means when screen transition needs.
+        // Comment and run this, you will see heartbeat will never stops.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0, execute: {
+            print("Heart Attack Stopped")
+            splashView.finishHeartBeatAnimation()
+        })
+    }
+    
     func splitViewController(
              _ splitViewController: UISplitViewController,
              collapseSecondary secondaryViewController: UIViewController,
